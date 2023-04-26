5d5ce5a6c30c7fc5f645091cda4f91d895baff1a
Abaci2424 <dymuraoksana@gmail.com>
2023-04-26 09:53:16 +0300

    поворот

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ main.appendChild(tetris);
 let exselPos = document.getElementsByClassName('excel');
 let i = 0;
 
-for (let y=18; y>0; y--) {
-  for (let x=1; x<11; x++) {
+for (let y = 18; y > 0; y--) {
+  for (let x = 1; x < 11; x++) {
     exselPos[i].setAttribute('posX', x);
     exselPos[i].setAttribute('posY', y);
     i++;
@@ -28,56 +28,256 @@ let mainArr = [
 
   // I
   [
-  [0,1],
-  [0,2],
-  [0,3],
-],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    //doner 90
+    [
+      [-1, 1],
+      [0, 0],
+      [1, -1],
+      [2, -2],
+    ],
+    //doner 180
+    [
+      [1, -1],
+      [0, 0],
+      [-1, 1],
+      [-2, 2],
+    ],
+    //doner 270
+    [
+      [-1, 1],
+      [0, 0],
+      [1, -1],
+      [2, -2],
+    ],
+    //doner 360
+    [
+      [1, -1],
+      [0, 0],
+      [-1, 1],
+      [-2, 2],
+    ],
+  ],
 
-// kare
-[
-  [1,0],
-  [0,1],
-  [1,1]
-],
-// L=>
-[
-  [1,0],
-  [0,1],
-  [0,2]
-],
-// <=L
-[
-  [1,0],
-  [1,1],
-  [1,2]
-],
-// <=Z
-[
-  [1,0],
-  [-1,1],
-  [0,1]
-],
-// Z=>
-[
-  [1,0],
-  [1,1],
-  [2,1]
-],
-// T
-[
-  [1,0],
-  [2,0],
-  [1,1]
-]
+  // kare
+  [
+    [1, 0],
+    [0, 1],
+    [1, 1],
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+    ],
+    //doner 180
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+    ],
+    //doner 270
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+    ],
+    //doner 360
+    [
+      [1, -1],
+      [0, 0],
+      [-1, 1],
+      [-2, 2],
+    ],
+  ],
+  // L=>
+  [
+    [1, 0],
+    [0, 1],
+    [0, 2],
+
+    //doner 90
+    [
+      [0, 0],
+      [-1, 1],
+      [1, 0],
+      [2, -1],
+    ],
+    //doner 180
+    [
+      [1, -1],
+      [1, -1],
+      [-1, 0],
+      [-1, 0],
+    ],
+    //doner 270
+    [
+      [-1, 0],
+      [0, -1],
+      [2, -2],
+      [1, -1],
+    ],
+    //doner 360
+    [
+      [0, -1],
+      [0, -1],
+      [-2, 0],
+      [-2, 0],
+    ],
+  ],
+  // <=L
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    //doner 90
+    [
+      [0, 0],
+      [0, 0],
+      [1, -1],
+      [-1, -1]
+    ],
+    //doner 180
+    [
+      [0, -1],
+      [-1, 0],
+      [-2, 1],
+      [1, 0]
+    ],
+    //doner 270
+    [
+      [2, 0],
+      [0, 0],
+      [1, -1],
+      [1, -1]
+    ],
+    //doner 360
+    [
+      [0, -1],
+      [0, -1],
+      [-2, 0],
+      [-2, 0]
+    ],
+  ],
+  // <=Z
+  [
+    [1, 0],
+    [-1, 1],
+    [0, 1],
+    //doner 90
+    [
+      [0, -1],
+      [-1, 0],
+      [2, -1],
+      [1, 0]
+    ],
+    //doner 180
+    [
+      [0, 0],
+      [1, -1],
+      [-2, 0],
+      [-1, -1]
+    ],
+    //doner 270
+    [
+      [0, -1],
+      [-1, 0],
+      [2, -1],
+      [1, 0]
+    ],
+    //doner 360
+    [
+      [0, 0],
+      [1, -1],
+      [-2, 0],
+      [-1, -1]
+    ],
+  ],
+  // Z=>
+  [
+    [1, 0],
+    [1, 1],
+    [2, 1],
+    //doner 90
+    [
+      [2, -1],
+      [0, 0],
+      [1, -1],
+      [-1, 0]
+    ],
+    //doner 180
+    [
+      [-2, 0],
+      [0, -1],
+      [-1, 0],
+      [1, -1]
+    ],
+    //doner 270
+    [
+      [2, -1],
+      [0, 0],
+      [1, -1],
+      [-1, 0]
+    ],
+    //doner 360
+    [
+      [-2, 0],
+      [0, -1],
+      [-1, 0],
+      [1, -1]
+    ],
+  ],
+  // T
+  [
+    [1, 0],
+    [2, 0],
+    [1, 1],
+    //doner 90
+    [
+      [1, -1],
+      [0, 0],
+      [0, 0],
+      [0, 0]
+    ],
+    //doner 180
+    [
+      [0, 0],
+      [-1, 0],
+      [-1, 0],
+      [1, -1]
+    ],
+    //doner 270
+    [
+      [1, -1],
+      [1, -1],
+      [1, -1],
+      [0, 0]
+    ],
+    //doner 360
+    [
+      [-2, 0],
+      [0, -1],
+      [0, -1],
+      [-1, -1]
+    ],
+  ],
 ]
 
-currentFigure = 0;
-figureBody = 0;
+let currentFigure = 0;
+let figureBody = 0;
+let rotate = 1;
+
 
 function create() {
   function getRandom() {
-    return Math.round(Math.random()*(mainArr.length-1))
+    return Math.round(Math.random() * (mainArr.length - 1))
   }
+
+  rotate = 1;
   currentFigure = getRandom();
 
   figureBody = [
@@ -87,7 +287,7 @@ function create() {
     document.querySelector(`[posX = "${x + mainArr[currentFigure][2][0]}"][posY = "${y + mainArr[currentFigure][2][1]}"]`),
   ]
 
-  for(let i = 0; i<figureBody.length; i++) {
+  for (let i = 0; i < figureBody.length; i++) {
     figureBody[i].classList.add('figure');
   }
 }
@@ -106,15 +306,15 @@ function move() {
     [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],
   ];
 
-  for(let i = 0; i<coordinates.length; i++) {
-    if(coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY ="${coordinates[i][1] - 1}"]`).classList.contains('set')) {
+  for (let i = 0; i < coordinates.length; i++) {
+    if (coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY ="${coordinates[i][1] - 1}"]`).classList.contains('set')) {
       moveFlag = false;
       break;
     }
   }
 
-  if(moveFlag) {
-    for(let i = 0; i < figureBody.length; i++) {
+  if (moveFlag) {
+    for (let i = 0; i < figureBody.length; i++) {
       figureBody[i].classList.remove('figure');
     }
 
@@ -125,11 +325,11 @@ function move() {
       document.querySelector(`[posX = "${coordinates[3][0]}"][posY ="${coordinates[3][1] - 1}"]`),
     ];
 
-    for(let i = 0; i < figureBody.length; i++) {
+    for (let i = 0; i < figureBody.length; i++) {
       figureBody[i].classList.add('figure');
     }
   } else {
-    for(let i = 0; i < figureBody.length; i++) {
+    for (let i = 0; i < figureBody.length; i++) {
       figureBody[i].classList.remove('figure');
       figureBody[i].classList.add('set');
     }
@@ -140,6 +340,85 @@ function move() {
 
 
 let interval = setInterval(() => {
-   move();
-}, 300); 
+  move();
+}, 300);
+
+window.addEventListener('keydown', function (e) {
+  let coordinates1 = [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')];
+  let coordinates2 = [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')];
+  let coordinates3 = [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')];
+  let coordinates4 = [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')];
+
+  function getNewState(a) {
+
+    flag = true;
+
+    let figureNew = [
+      document.querySelector(`[posX = "${+coordinates1[0] + a}"][posY = "${coordinates1[1]}"]`),
+      document.querySelector(`[posX = "${+coordinates2[0] + a}"][posY = "${coordinates2[1]}"]`),
+      document.querySelector(`[posX = "${+coordinates3[0] + a}"][posY = "${coordinates3[1]}"]`),
+      document.querySelector(`[posX = "${+coordinates4[0] + a}"][posY = "${coordinates4[1]}"]`),
+    ]
+
+    for (let i = 0; i < figureNew.length; i++) {
+      if (!figureNew[i] || figureNew[i].classList.contains('set')) {
+        flag = false;
+      }
+    }
+    if (flag == true) {
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.remove('figure');
+      }
+      figureBody = figureNew;
+      
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.add('figure');
+      }
+    }
+  }
+
+  if (e.keyCode == 37) {
+    getNewState(-1);
+  } else if (e.keyCode == 39) {
+    getNewState(1);
+  } else if (e.keyCode == 40) {
+    move();
+  } else if (e.keyCode == 38) {
+
+
+    flag = true;
+
+    let figureNew = [
+      document.querySelector(`[posX = "${+coordinates1[0] + mainArr[currentFigure][rotate + 2][0][0]}"][posY = "${+coordinates1[0] + mainArr[currentFigure][rotate + 2][0][1]}"]`),
+      document.querySelector(`[posX = "${+coordinates2[0] + mainArr[currentFigure][rotate + 2][1][0]}"][posY = "${+coordinates2[0] + mainArr[currentFigure][rotate + 2][1][1]}"]`),
+      document.querySelector(`[posX = "${+coordinates3[0] + mainArr[currentFigure][rotate + 2][2][0]}"][posY = "${+coordinates3[0] + mainArr[currentFigure][rotate + 2][2][1]}"]`),
+      document.querySelector(`[posX = "${+coordinates4[0] + mainArr[currentFigure][rotate + 2][3][0]}"][posY = "${+coordinates4[0] + mainArr[currentFigure][rotate + 2][3][1]}"]`),
+    ]
+
+    for (let i = 0; i < figureNew.length; i++) {
+      if (!figureNew[i] || figureNew[i].classList.contains('set')) {
+        flag = false;
+      }
+    }
+
+    if (flag == true) {
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.remove('figure');
+      }
+
+      figureBody = figureNew;
+      
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.add('figure');
+      }
+
+      if (rotate < 4) {
+        rotate++;
+      } else {
+        rotate = 1;
+      }
+
+    }
+  }
+})
 
